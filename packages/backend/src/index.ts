522a81bc52be1aaa9bb420b309c889039dbf5035
DonnyZhao <do.zhao@cmcmarkets.com>
2024-11-11 20:33:10 +1100

    Revert "Feature/validation 1"

--- packages/backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-// src/index.ts
-import app from './app';
-
-// TODO: 
-const port = process.env.PORT || 9000;
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
